5aa2513fc6a60a86b8e6a2e8cbf2a1a8ca9fb6e6
Julie Braford <jbraford@css.edu>
2019-11-03 19:04:54 -0600

    Request 407 - Added a trade URL reader class and called in the main program

--- SingleResponsibilityPrinciple/Program.cs
+++ SingleResponsibilityPrinciple/Program.cs
@@ -13,7 +13,7 @@ namespace SingleResponsibilityPrinciple
 
             var logger = new ConsoleLogger();
             var tradeValidator = new SimpleTradeValidator(logger);
-            var tradeDataProvider = new StreamTradeDataProvider(tradeStream);
+            var tradeDataProvider = new URLTradeDataProvider("http://faculty.css.edu/tgibbons/trades4.txt");
             var tradeMapper = new SimpleTradeMapper();
             var tradeParser = new SimpleTradeParser(tradeValidator, tradeMapper);
             var tradeStorage = new AdoNetTradeStorage(logger);
